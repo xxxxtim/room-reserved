ff5140e394189446fe3d1b795adcc09ed6f93214
xxxxtim <xxxxtim@hotmail.com>
2020-08-12 20:05:26 +0800

    feat: 首頁房型切板

---
+++ src/components/RoomStyles.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div>
+      <div class="rooms-container">
+        <div>
+          <div>房型1照片</div>
+          <p>顯示價位</p>
+        </div>
+        <div>
+          <div>房型2照片</div>
+          <p>顯示價位</p>
+        </div>
+        <div>
+          <div>房型3照片</div>
+          <p>顯示價位</p>
+        </div>
+      </div>
+      <div class="rooms-container">
+        <div>
+          <div>房型4照片</div>
+          <p>顯示價位</p>
+        </div>
+        <div>
+          <div>房型5照片</div>
+          <p>顯示價位</p>
+        </div>
+        <div>
+          <div>房型6照片</div>
+          <p>顯示價位</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
