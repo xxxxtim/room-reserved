b48f41b516044f3a98d803891a00b3fbfd5ee404
xxxxtim <xxxxtim@hotmail.com>
2020-08-20 20:18:57 +0800

    feat: web-slice

--- src/views/SingleRoom.vue
+++ src/views/SingleRoom.vue
@@ -11,112 +11,116 @@
       <div class="msg-left">
         <div class="context-left">
           <h1>{{room[0].name}}</h1>
-          <div>
-            <p>房客人數限制： 1~1 人</p>
-            <p>床型：單人床</p>
-            <p>衛浴數量： 1 間</p>
-            <p>房間大小： 18 平方公尺</p>
+          <div class="roomType">
+            <p>房客人數限制： {{room[0].descriptionShort.GuestMin}}~{{room[0].descriptionShort.GuestMax}} 人</p>
+            <p>床型：{{room[0].descriptionShort.Bed[0]}}</p>
+            <p>衛浴數量：{{room[0].descriptionShort['Private-Bath']}} 間</p>
+            <p>房間大小： {{room[0].descriptionShort.Footage}} 平方公尺</p>
           </div>
-          <p>Single Room is only reserved for one guest. There is a bedroom with a single size bed and a private bathroom. Everything you need prepared for you: sheets and blankets, towels, soap and shampoo, hairdryer are provided. In the room there is AC and of course WiFi.</p>
+          <!-- <p>Single Room is only reserved for one guest. There is a bedroom with a single size bed and a private bathroom. Everything you need prepared for you: sheets and blankets, towels, soap and shampoo, hairdryer are provided. In the room there is AC and of course WiFi.</p> -->
+          <p class="roomDescript">{{room[0].description}}</p>
           <span>\\\</span>
           <div class="time-container">
             <div>
               <p>check in</p>
-              <span>15:00 — 21:00</span>
+              <!-- <span>15:00 — 21:00</span> -->
+              <span
+                class="time"
+              >{{room[0].checkInAndOut.checkInEarly}} — {{room[0].checkInAndOut.checkInLate}}</span>
             </div>
             <div>
               <p>check out</p>
-              <span>10:00</span>
+              <span class="time">{{room[0].checkInAndOut.checkOut}}</span>
             </div>
           </div>
           <!-- 這邊可以拆component -->
-          <div class="icons-container">
+          <div class="prompt">
             <div class="icons">
-              <div>
-                <div>
+              <div class="iconsWrapper">
+                <div :class="{ iconActive: !!room[0].amenities['Wi-Fi'] }">
                   <font-awesome-icon icon="wifi" />
                 </div>
-                <div>
+                <div :class="{ iconActive: !!room[0].amenities.Breakfast }">
                   <font-awesome-icon icon="utensils" />
                 </div>
-                <div>
+                <div :class="{ iconActive: !!room[0].amenities['Mini-Bar']}">
                   <font-awesome-icon icon="glass-martini-alt" />
                 </div>
-                <div>
+                <div :class="{ iconActive: !!room[0].amenities['Room-Service']}">
                   <font-awesome-icon icon="bell" />
                 </div>
               </div>
               <div>
-                <div>
+                <div :class="{ iconActive: !!room[0].amenities['Wi-Fi']}">
                   <span class="icon-text">wifi</span>
                 </div>
-                <div>
+                <div :class="{ iconActive: !!room[0].amenities.Breakfast }">
                   <span class="icon-text">早餐</span>
                 </div>
-                <div>
+                <div :class="{ iconActive: !!room[0].amenities['Mini-Bar']}">
                   <span class="icon-text">Mini Bar</span>
                 </div>
-                <div>
+                <div :class="{ iconActive: !!room[0].amenities['Room-Service']}">
                   <span class="icon-text">Room service</span>
                 </div>
               </div>
             </div>
             <div class="icons">
-              <div>
-                <div>
+              <div class="iconsWrapper">
+                <div :class="{ iconActive: !!room[0].amenities.Television }">
                   <font-awesome-icon icon="tty" />
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Air-Conditioner']}">
                   <font-awesome-icon icon="fan" />
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities.Refrigerator}">
                   <font-awesome-icon icon="dice-six" />
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities.Sofa}">
                   <font-awesome-icon icon="couch" />
                 </div>
               </div>
               <div>
-                <div>
+                <div :class="{ iconActive: !!room[0].amenities.Television }">
                   <span class="icon-text">電話</span>
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Air-Conditioner']}">
                   <span class="icon-text">空調</span>
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities.Refrigerator}">
                   <span class="icon-text">冰箱</span>
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities.Sofa}">
                   <span class="icon-text">沙發</span>
                 </div>
               </div>
             </div>
             <div class="icons">
-              <div>
-                <div>
+              <div class="iconsWrapper">
+                <div :class="{iconActive:!!room[0].amenities['Great-View']}">
                   <font-awesome-icon icon="mountain" />
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Smoke-Free']}">
                   <font-awesome-icon icon="smoking-ban" />
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Child-Friendly']}">
                   <font-awesome-icon icon="baby" />
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Pet-Friendly']}">
                   <font-awesome-icon icon="dog" />
                 </div>
               </div>
               <div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Great-View']}">
                   <span class="icon-text">漂亮的視野</span>
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Smoke-Free']}">
                   <span class="icon-text">禁止吸煙</span>
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Child-Friendly']}">
                   <span class="icon-text">適合兒童</span>
                 </div>
-                <div>
+                <div :class="{iconActive:!!room[0].amenities['Pet-Friendly']}">
                   <span class="icon-text">寵物攜帶</span>
                 </div>
               </div>
@@ -194,6 +198,7 @@ export default {
   background-size: cover;
   background-position: center;
   cursor: pointer;
+  flex: 8 8 auto;
 }
 .banner-container {
   display: flex;
@@ -205,17 +210,18 @@ export default {
   background-size: cover;
   background-position: center;
   cursor: pointer;
+  flex: 8 1 auto;
 }
 .time-container {
   display: flex;
   flex-direction: row;
 }
-.icons-container {
+.prompt {
   background: #f0f0f0;
   display: flex;
   flex-direction: row;
   color: #6c757d;
-  width: 482px;
+  // width: 482px;
   height: 220px;
   justify-content: space-around;
   align-items: center;
@@ -238,10 +244,38 @@ export default {
 }
 .msg-middle {
   width: 130px;
-  flex: 1 1 auto;
+  flex: 3 1 auto;
 }
 .msg-right {
   width: 385px;
   flex: 1 1 auto;
+}
+.iconActive {
+  color: black;
+}
+.iconsWrapper {
+  text-align: center;
+}
+.roomDescript {
+  font-family: NotoSansCJKtc-Light;
+  letter-spacing: 1.25px;
+  text-align: justify;
+  line-height: 20px;
+}
+h1 {
+  font-size: 2.5rem;
+  font-family: NotoSansCJKtc-Medium;
+  /* font-size: 36px; */
+  // color: #000000;
+  letter-spacing: 3.76px;
+}
+.roomType {
+  font-family: NotoSansCJKtc-Light;
+  letter-spacing: 1.46px;
+  line-height: 31px;
+}
+.time {
+  font-size: 21px;
+  letter-spacing: 2.19px;
 }
 </style>
