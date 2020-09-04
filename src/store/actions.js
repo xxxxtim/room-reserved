66a97c245dee1d1e385e02f4a1264e43db3c1ad4
xxxxtim <xxxxtim@hotmail.com>
2020-09-05 02:46:44 +0800

    style: 更改post api 寫法
    1.修改async/then/catch 混用問題
    2. 修正為 async /try/catcch

--- src/store/actions.js
+++ src/store/actions.js
@@ -30,33 +30,43 @@ export default {
 
     },
 
-    // async 和思then 寫法要改
-    async postBooking({ commit }, { roomId, name, tel, booking }) {
-        console.log(roomId, name, tel, booking)
-        await axios.post(`https://challenge.thef2e.com/api/thef2e2019/stage6/room/${roomId}`,
-            {
-                name: name,
-                tel: tel,
-                date: booking
-            },
-            {
-                headers: {
-                    Authorization:
-                        "Bearer IOdf0UIIYuWAaUqB84OcRb0NJYEBDWYUFygTLF2N8MV1WD5J6GK4HDHnGwUL"
-                }
-            })
-            .then((res) => {
 
-                // console.log(res.data);
-                commit('postSuccess', res);
-            }
-            )
-            .catch((error) => {
-                commit('postFail', error)
-                // console.log('yachen666')
-                console.log(error)
-            }
-            )
+    async postBooking({ commit }, { roomId, name, tel, booking }) {
+        // console.log(roomId, name, tel, booking)
+        try {
+            const res = await axios.post(`https://challenge.thef2e.com/api/thef2e2019/stage6/room/${roomId}`,
+                {
+                    name: name,
+                    tel: tel,
+                    date: booking
+                },
+                {
+                    headers: {
+                        Authorization:
+                            "Bearer IOdf0UIIYuWAaUqB84OcRb0NJYEBDWYUFygTLF2N8MV1WD5J6GK4HDHnGwUL"
+                    }
+                })
+            commit('postSuccess', res);
+            console.log('bannan');
+            console.log(res.data);
+
+        }
+        catch (err) {
+            commit('postFail', err)
+            console.log('orange')
+        }
+        // .then((res) => {
+
+        //     // console.log(res.data);
+        //     commit('postSuccess', res);
+        // }
+        // )
+        // .catch((error) => {
+        //     commit('postFail', error)
+        //     // console.log('yachen666')
+        //     console.log(error)
+        // }
+        // )
     }
 
 
