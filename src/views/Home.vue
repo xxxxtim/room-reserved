cb34968f10ca63f2812c2c559ddaf00b4293534b
xxxxtim <xxxxtim@hotmail.com>
2020-09-18 18:00:08 +0800

    fix: banner 滿板

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="main-container">
+<div class="main-container">
     <div class="logo">
-      <Logo />
+        <Logo />
     </div>
     <div>
-      <Carousel />
+        <Carousel />
     </div>
     <div class="rooms">
-      <RoomStyles />
+        <RoomStyles />
     </div>
-  </div>
+</div>
 </template>
 
 <script>
@@ -21,34 +21,35 @@ import Logo from "@/components/Logo.vue";
 import store from "@/store";
 
 export default {
-  name: "Home",
-  components: {
-    // HelloWorld
-    RoomStyles,
-    Carousel,
-    Logo
-  },
+    name: "Home",
+    components: {
+        // HelloWorld
+        RoomStyles,
+        Carousel,
+        Logo
+    },
 
-  created() {
-    // axios
-    //   .get("https://challenge.thef2e.com/api/thef2e2019/stage6/rooms", {
-    //     headers: {
-    //       Authorization:
-    //         "Bearer IOdf0UIIYuWAaUqB84OcRb0NJYEBDWYUFygTLF2N8MV1WD5J6GK4HDHnGwUL"
-    //     }
-    //   })
-    //   .then(response => (this.roomsStyles = response.data.items));
-    store.dispatch("getRoomsStyleAPI");
-  },
-  mounted() {
-    // console.log(33333333);
-  }
+    created() {
+        // axios
+        //   .get("https://challenge.thef2e.com/api/thef2e2019/stage6/rooms", {
+        //     headers: {
+        //       Authorization:
+        //         "Bearer IOdf0UIIYuWAaUqB84OcRb0NJYEBDWYUFygTLF2N8MV1WD5J6GK4HDHnGwUL"
+        //     }
+        //   })
+        //   .then(response => (this.roomsStyles = response.data.items));
+        store.dispatch("getRoomsStyleAPI");
+    },
+    mounted() {
+        // console.log(33333333);
+    }
 };
 </script>
+
 <style lang="scss" scoped>
 .main-container {
-  position: relative;
-  max-width: 1440px;
-  // margin: 0 auto;
+    position: relative;
+    // max-width: 1440px;
+    // margin: 0 auto;
 }
 </style>
